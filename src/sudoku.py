976ddadf5deb16b4abca93e63921e1439953c85b
Marc Lopez <marc.rainier.lopez@gmail.com>
2015-03-31 22:55:24 -0400

    Refactor

--- src/sudoku.py
+++ src/sudoku.py
@@ -5,30 +5,20 @@ Created on Mar 30, 2015
 
 '''
 
-
-def is_segment_valid(segment, expected_segment_sum):
-    if len(segment) != len(set(segment)) or sum(segment) != expected_segment_sum:
-        return False
-    return True
+def is_segment_valid(segment):
+    return set(segment) == set(range(1,10))
 
 def is_valid(board):
     board_size = len(board)
-    expected_segment_sum = sum(range(1, 10))
-    row = []
-    col = []
-    square = []
     for i in range(board_size):
+        row = col = square = []
         for j in range(board_size):
             row.append(board[i][j])
             col.append(board[j][i])
             square.append(board[((i / 3) * 3) + (j / 3)][((i % 3) * 3) + (j % 3)])
         
-        if not all([is_segment_valid(segment, expected_segment_sum) for segment in [row, col, square]]):
+        if not all([is_segment_valid(segment) for segment in [row, col, square]]):
             return False
-        
-        row = []
-        col = []
-        square = []
         
     return True
 
